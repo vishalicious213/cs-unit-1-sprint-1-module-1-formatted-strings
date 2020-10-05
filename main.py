112dda4e4b3ab3feaf5edbfad53054c9cf6e39c5
Vish Singh <vishalicious213@gmail.com>
2020-10-05 21:23:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+1. Assign three different types of data to the three variables "a", "b", and "c".
+
+2. Use a format string to inject the data from your three variables into the string.
+"""
+# Modify the code below to meet the requirements above.
+a = 213
+b = 2.13
+c = "string"
+
+print("a = %d, b = %.2f, c = %s" % (a, b, c))
